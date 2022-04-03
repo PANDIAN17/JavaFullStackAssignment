e8aaf5db687aaabed437f187f4e83657d36bfa72
PANDIAN17 <100667964+PANDIAN17@users.noreply.github.com>
2022-04-03 22:20:35 +0530

    modified the code

--- src/com/FileFinal.java
+++ src/com/FileFinal.java
@@ -18,11 +18,11 @@ public class FileFinal {
 					System.out.println("Please choose one of the options below:\n"+
 							"*********************************************\n"+
 										"1.Return the current file names\n"+
-										"2.Add/Delete/Search/Copy/Read/Display a file");
+										"2.Add/Delete/Search/Exit");
 		Scanner sc=new Scanner(System.in);
 		System.out.println("File operations");
 		while(true) {
-			System.out.println("Enter your choice 1.Write 2.Read 3.Copy 4.Delete 5.Exit 6.Display 7.search");
+			System.out.println("Enter your choice 1.Add 2.Delete 3.search 4.Exit ");
 			int choice=sc.nextInt();
 			switch(choice) {
 			case 1: 
@@ -32,34 +32,16 @@ public class FileFinal {
 			
 			//byte
 			
-			FileOutputStream fos=new FileOutputStream("C:\\Users\\jpand\\Software\\Test\\a.txt",true);
+			FileOutputStream fos=new FileOutputStream("C:\\Users\\jpand\\Software\\Test\\p.txt",true);
 			
 			byte b[]=s.getBytes();
 			fos.write(b);
 			fos.close();
 			break;
-			case 2: 
-				FileInputStream fis=new FileInputStream("C:\\Users\\jpand\\Software\\Test\\a.txt");
-				int i=0;
-				while((i=fis.read())!=-1) {
-					System.out.print((char)i);
-						//fos.close();
-				}
-					break;
-				case 3:
-					FileOutputStream fos1 = new FileOutputStream("C:\\Users\\jpand\\Software\\Test\\b.txt");
-
-					FileInputStream fis1 = new FileInputStream("C:\\Users\\jpand\\Software\\Test\\a.txt");
-					
-					int j=0;
-					while ((j=fis1.read()) != -1) {
-						fos1.write(j);
-						
-				}
-					break;
-				case 4:
-					System.out.println("Please enter the name of the file to be deleted");
-					String path = "C:\\Users\\jpand\\Desktop\\Test\\z.txt";
+	
+		 case 2:
+			System.out.println("Please enter the name of the file to be deleted");
+					String path = "C:\\Users\\jpand\\Software\\Test\\z.txt";
 					String finalfile = path;
 					File f = new File(finalfile);
 					boolean flag1 = f.delete();
@@ -71,23 +53,8 @@ public class FileFinal {
 						
 					}
 					//f.close();
-					break;
-				case 5:
-					System.exit(0);
-					break;
-				case 6:
-					
-					String path1 = "C:\\Users\\jpand\\Software\\Test\\";
-					File  f1 = new File(path1);
-					File filen[]= f1.listFiles();
-					for(File ff:filen) {
-						String s1 = ff+"";
-						String ss[] =s1.split("\\\\");
-						System.out.println(ss[ss.length - 1]);
-					}
-					break;
-					
-				case 7:
+					break;	
+				case 3:
 					String path2 = "C:\\Users\\jpand\\Software\\Test\\";
 					File  f2 = new File(path2);
 					System.out.println("Enter  the file name");
@@ -111,7 +78,10 @@ public class FileFinal {
 							else {
 								System.out.println("File is not found ");
 							}
-							break;				
+							break;
+				case 4:
+					System.exit(0);
+					break;			
 			}
 	}
 
